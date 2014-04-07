1c524a7269e8c673ebeef97e8f0f705b36bbdf35
Dimitri Belopopsky <dimitri@belopopsky.com>
2014-04-08 00:06:02 +0200

    Commit all the changes Added menu, logos, etc

---
+++ Assets/Scripts/DelayedNextScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayedNextScene : MonoBehaviour {
+  float now;
+	// Use this for initialization
+	void Start () {
+    now = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (Time.time - now > 3 || Input.GetKeyDown("space"))
+      {
+        Application.LoadLevel("MainMenu");
+      }
+	}
+}
---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButton : MonoBehaviour {
+	public float x;
+	public float y;
+	public float h;
+	public float w;
+	Transform trans;
+	public Texture texture;
+	Rect pos;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnGUI() {
+		pos.x = x;
+		pos.y = y;
+		pos.height = h;
+		pos.width = w;
+		GUI.backgroundColor = Color.clear;
+		if (GUI.Button (pos, texture)) {
+			Application.LoadLevel ("TestLevel");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
